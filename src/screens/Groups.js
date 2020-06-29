9df056284cc0e71528799d94f502bd0b2e8b000d
chandra655 <chandra.penugonda@unacademy.com>
2020-06-29 23:02:07 +0530

    add navigator.setOptions for groups screen

--- src/screens/Groups.js
+++ src/screens/Groups.js
@@ -1,10 +1,48 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {StatusBar} from 'react-native';
+import styled from 'styled-components/native';
 
-export default function Groups() {
+export default function Groups({navigation}) {
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Button>
+          <Image
+            source={{
+              uri:
+                'https://img.icons8.com/ios/30/000000/logout-rounded-left.png',
+            }}
+          />
+        </Button>
+      ),
+      headerRight: () => (
+        <Button>
+          <Image
+            source={{
+              uri: 'https://img.icons8.com/pastel-glyph/64/000000/plus.png',
+            }}
+          />
+        </Button>
+      ),
+    });
+  }, [navigation]);
   return (
-    <View>
-      <Text>Grouups</Text>
-    </View>
+    <Container>
+      <Text>Groups</Text>
+    </Container>
   );
 }
+
+const Container = styled.View``;
+const Text = styled.Text``;
+const Button = styled.TouchableOpacity`
+  width: 30px;
+  height: 30px;
+  margin: 0 10px;
+  justify-content: center;
+  align-items: center;
+`;
+const Image = styled.Image`
+  width: 100%;
+  height: 100%;
+`;
