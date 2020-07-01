ae5e1aa99b39e700aed3ba99ca8a055584655e79
chandra655 <chandra.penugonda@unacademy.com>
2020-07-01 23:57:26 +0530

    get groups from firebase and dispaly using flatlist

--- src/screens/Groups.js
+++ src/screens/Groups.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
 import styled from 'styled-components/native';
+import {firestore} from '../firebase/firebase';
+import {FlatList} from 'react-native-gesture-handler';
+
+import Text from '../shared/Text';
+import colors from '../utils/colors';
 
 export default function Groups({navigation}) {
+  const [groups, setGroups] = React.useState([]);
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerLeft: () => (
-        <Button>
+        <Button activeOpacity={1} onPress={() => {}}>
           <Image
             source={{
               uri:
@@ -16,7 +21,11 @@ export default function Groups({navigation}) {
         </Button>
       ),
       headerRight: () => (
-        <Button>
+        <Button
+          activeOpacity={1}
+          onPress={() => {
+            navigation.navigate('AddGroup');
+          }}>
           <Image
             source={{
               uri: 'https://img.icons8.com/pastel-glyph/64/000000/plus.png',
@@ -26,15 +35,45 @@ export default function Groups({navigation}) {
       ),
     });
   }, [navigation]);
+  React.useEffect(() => {
+    try {
+      firestore.collection('groups').onSnapshot(function(querySnapshot) {
+        const tempGroups = [];
+        querySnapshot.forEach(function(doc) {
+          const {groupName, groupId, thumbnail} = doc.data();
+          tempGroups.push({name: groupName, id: groupId, thumbnail});
+        });
+        setGroups(tempGroups);
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  }, []);
+  const renderGroup = group => (
+    <Group>
+      <Thumbnail>
+        <GroupIcon
+          source={{
+            uri: group.thumbnail,
+          }}
+        />
+      </Thumbnail>
+      <GroupName small>{group.name}</GroupName>
+    </Group>
+  );
   return (
     <Container>
-      <Text>Groups</Text>
+      <FlatList
+        data={groups}
+        renderItem={({item}) => renderGroup(item)}
+        keyExtractor={item => String(item.id)}
+      />
     </Container>
   );
 }
 
 const Container = styled.View``;
-const Text = styled.Text``;
+
 const Button = styled.TouchableOpacity`
   width: 30px;
   height: 30px;
@@ -45,4 +84,30 @@ const Button = styled.TouchableOpacity`
 const Image = styled.Image`
   width: 100%;
   height: 100%;
+`;
+
+const Thumbnail = styled.View`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${colors.uaStudiosGreen};
+`;
+
+const GroupIcon = styled.Image`
+  width: 30px;
+  height: 30px;
+`;
+
+const Group = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 10px 20px;
+  border: 1px solid ${colors.lighishGray};
+`;
+
+const GroupName = styled(Text)`
+  padding-left: 20px;
+  color: ${colors.black};
 `;
