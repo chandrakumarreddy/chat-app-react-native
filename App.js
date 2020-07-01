4880c9ececaa9abd21bc198c7592a411046a1aef
chandra655 <chandra.penugonda@unacademy.com>
2020-07-01 23:57:40 +0530

    fix firebase base64 issue

--- App.js
+++ App.js
@@ -1,3 +1,13 @@
+import {decode, encode} from 'base-64';
+
+if (!global.btoa) {
+  global.btoa = encode;
+}
+
+if (!global.atob) {
+  global.atob = decode;
+}
+
 import React from 'react';
 import Main from './src/navigation/Main';
 
